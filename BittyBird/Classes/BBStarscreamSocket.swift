83d6283038e4f5c109bd3bb1783776ed0e0427f6
Nick Eneboe <neneboe@gmail.com>
2018-07-12 17:59:07 -0700

    made BBStarscreamSocket open

--- BittyBird/Classes/BBStarscreamSocket.swift
+++ BittyBird/Classes/BBStarscreamSocket.swift
@@ -7,8 +7,8 @@
 
 import Starscream
 
-class BBStarscreamSocket: WebSocket, BBWebSocket {
-  required init(url: URL) {
+open class BBStarscreamSocket: WebSocket, BBWebSocket {
+  required public init(url: URL) {
     var request = URLRequest(url: url)
     request.timeoutInterval = 5
     super.init(request: request, protocols: nil)
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ BittyBird is available through [CocoaPods](https://cocoapods.org). To install
 it, simply add the following line to your Podfile:
 
 ```ruby
-pod 'BittyBird', '~> 0.1.1'
+pod 'BittyBird', '~> 0.1.2'
 ```
 
 ## Requirements
